ffa292ab122c1c4d644202f03df97f0c153917a4
Artur <divermsurartur@gmail.com>
2025-11-30 16:57:04 +0100

    add/dir

---
+++ backend/queries.sql
@@ -0,0 +1,36 @@
+
+
+
+CREATE TABLE users (
+  id_user serial NOT NULL PRIMARY KEY, 
+  username varchar(45) NOT NULL UNIQUE, 
+  email varchar(100) NOT NULL UNIQUE,
+  password varchar NOT NULL,
+  role varchar(15) NOT NULL
+);
+
+CREATE TABLE providers (
+    id_provider     SERIAL PRIMARY KEY,
+    companyname     VARCHAR(100) NOT NULL,
+    website         VARCHAR(255),
+    img             VARCHAR(255)
+);
+
+CREATE TABLE products (
+    id_product    SERIAL PRIMARY KEY,
+    name          VARCHAR(100) NOT NULL,
+    price         DECIMAL(10,2) NOT NULL,
+    description   TEXT,
+    img           VARCHAR(255),
+    id_provider   INTEGER NOT NULL,
+    FOREIGN KEY (id_provider) REFERENCES providers(id_provider)
+);
+
+
+CREATE TABLE favorites (
+    id_favorito   SERIAL PRIMARY KEY,
+    id_user       INTEGER NOT NULL,
+    id_product    INTEGER NOT NULL,
+    FOREIGN KEY (id_user) REFERENCES users(id_user),
+    FOREIGN KEY (id_product) REFERENCES products(id_product)
+);
