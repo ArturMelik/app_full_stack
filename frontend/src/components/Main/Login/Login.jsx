2a72913b5d995b76896dd7a4a997c72288a3d74f
Artur <divermsurartur@gmail.com>
2025-12-02 18:06:03 +0100

    try:login/auth

---
+++ frontend/src/components/Main/Login/Login.jsx
@@ -0,0 +1,70 @@
+import React from "react";
+import { useState } from "react";
+
+
+export default function Login() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState("");
+  const [success, setSuccess] = useState("");
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    setError("");
+    setSuccess("");
+
+    try {
+      const response = await fetch("http://localhost:5000/api/auth/login", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json"
+        },
+        body: JSON.stringify({ email, password })
+      });
+
+      const data = await response.json();
+
+      if (!response.ok) {
+        setError(data.message || "Error al iniciar sesión");
+        return;
+      }
+
+      // Guardar el token
+      localStorage.setItem("token", data.token);
+
+      setSuccess("Login correcto. Bienvenido!");
+      console.log("TOKEN:", data.token);
+
+    } catch (error) {
+      console.error(error);
+      setError("Error del servidor");
+    }
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <h2>Iniciar sesión</h2>
+
+      <input
+        type="email"
+        placeholder="Correo"
+        value={email}
+        onChange={(e) => setEmail(e.target.value)}
+        required
+      />
+
+      <input
+        type="password"
+        placeholder="Contraseña"
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
+        required
+      />
+
+      <button type="submit">Entrar</button>
+
+      {error && <p style={{ color: "red" }}>{error}</p>}
+      {success && <p style={{ color: "green" }}>{success}</p>}
+    </form>
+  );
+}
---
+++ frontend/src/components/Main/Main.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import {Route, Routes, Navigate} from "react-router-dom";
+
+const Main = () => {
+  return <div>Main</div>;
+};
+
+export default Main;
