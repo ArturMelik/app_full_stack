20d40b91745cb8d451bac552be6ea136cccffa4c
Artur <divermsurartur@gmail.com>
2025-12-09 14:18:04 +0100

    fix/env

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,7 +1,7 @@
 const { Pool } = require('pg');
 // require('dotenv').config();
 
-const isProduction = process.env.NODE_ENV === 'production';
+const isProduction = process.env.NODE_ENV === 'development';
 
 // Datos de conexión
 const pool = new Pool({ 
